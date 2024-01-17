a5a6df6b8762e08ba2e517d6240c860436efedfd
SekoZzzzzz <135242880+SekoZzzzzz@users.noreply.github.com>
2024-01-17 10:20:53 +0300

    Add files via upload

---
+++ scr/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Blockblack from "./blocks/BlockBlack";
+import Blockblue from "./blocks/BlockBlue";
+
+function Container(){
+  return(
+    <div className="container">
+      <Blockblack/>
+      <Blockblue/>
+    </div>
+  )
+}
+export default Container
